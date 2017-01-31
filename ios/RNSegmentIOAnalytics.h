2377b88d27ea1ae7bc974f1411c4d2055208df58
Jean Regisser <jean.regisser@gmail.com>
2017-01-31 11:31:18 +0100

    Added iOS hooks which allow configuring bundled integrations

--- ios/RNSegmentIOAnalytics.h
+++ ios/RNSegmentIOAnalytics.h
@@ -7,9 +7,20 @@
 
 #import "RCTBridgeModule.h"
 
+@class SEGAnalyticsConfiguration;
+
+typedef void (^OnSetupConfigFinishedBlock)(SEGAnalyticsConfiguration *config);
+typedef void (^OnSetupFinishedBlock)();
+
 /*
  * React native wrapper of the Segment.com's Analytics iOS SDK
  */
 @interface RNSegmentIOAnalytics : NSObject <RCTBridgeModule>
+
++ (OnSetupConfigFinishedBlock)onSetupConfigFinishedBlock;
++ (void)setOnSetupConfigFinishedBlock:(OnSetupConfigFinishedBlock)block;
+
++ (OnSetupFinishedBlock)onSetupFinishedBlock;
++ (void)setOnSetupFinishedBlock:(OnSetupFinishedBlock)block;
 
 @end
